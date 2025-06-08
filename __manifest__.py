f5cc06ae6aa15c3b5cf483b9d1b2f96673e1a5a2
sbs <admin@stridebs.com>
2025-06-08 19:52:45 +0300

    enhancements

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,26 @@
-    # -*- encoding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 {
-    "name" : "Journal Entry Report (Landscape)",
-    "version" : "18.0.0.1",
-    'author' : 'STRIDE BUSINESS SOLUTIONS',
-    'website' : 'https://www.stridebs.com',
-    "category" : "Accounting",
-    "description": """
-        This module enhances journal entries in Odoo by:
-        - Adding a landscape-format PDF report for journal entries.
-        - Enabling chatter (communication and logging) on journal entry records for better tracking.
-""",
-    'license':  "AGPL-3",
-    "depends" : ['account'],
-    "data" : [
+    'name': 'Journal Entry Report (Landscape)',
+    'version': '18.0.0.1',
+    'category': 'Accounting',
+    'summary': 'Print Journal Entries in Landscape PDF Format with Chatter Support',
+    'description': """
+            This module enhances the accounting workflow in Odoo by providing:
+
+            • A landscape-oriented PDF report for journal entries with a clean layout.
+            • Chatter integration on journal entries for better audit trails and collaboration.
+
+            Ideal for organizations that require formal journal entry prints or approval workflows.
+        """,
+    'author': 'STRIDE BUSINESS SOLUTIONS',
+    'website': 'https://www.stridebs.com',
+    'license': 'AGPL-3',
+    'depends': ['account', 'mail'],
+    'data': [
         'views/templates.xml',
         'views/report_actions.xml',
     ],
-    "images": ['static/description/icon.png'],
+    'images': ['static/description/icon.png'],
     'installable': True,
     'application': False,
     'auto_install': False,
--- models/account_move.py
+++ models/account_move.py
@@ -5,38 +5,6 @@ from email.utils import formataddr
 from odoo.exceptions import UserError
 
 
-class AccountMove(models.Model):
-    _name = 'account.move'
-    _inherit = ['account.move', 'mail.thread']
-
-    ref = fields.Char(tracking=True)
-    date = fields.Date(tracking=True)
-
-    def button_cancel(self):
-        res = super().button_cancel()
-        for move in self:
-            move.message_post(body=_('Journal Entry cancelled.'))
-        return res
-
-    def _get_default_from(self):
-        if self.env.user.email:
-            return formataddr((self.env.user.name, self.env.user.email))
-        raise UserError(_("Unable to send email, please configure the sender's email address or alias."))
-
-    def create_mail_message(self, body):
-        user = self.env.user
-        for move in self:
-            vals = {'message_type': 'notification',
-                    'author_id': user.partner_id.id,
-                    'date': datetime.now(),
-                    'email_from': self._get_default_from(),
-                    'model': 'account.move',
-                    'res_id': move.id,
-                    'subtype_id': 2,
-                    'body': body}
-            self.env['mail.message'].create(vals)
-
-
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
@@ -52,4 +20,37 @@ class AccountMoveLine(models.Model):
     @property
     def tax_names_str(self):
         return ', '.join(self.tax_ids.mapped('name')) if self.tax_ids else ''
+
+
+class AccountMove(models.Model):
+    _name = 'account.move'
+    _inherit = ['account.move', 'mail.thread']
+
+    ref = fields.Char(tracking=True)
+    date = fields.Date(tracking=True)
+
+    def button_cancel(self):
+        res = super().button_cancel()
+        for move in self:
+            move.message_post(body=_('Journal Entry cancelled.'))
+        return res
+
+    # def _get_default_from(self):
+    #     if self.env.user.email:
+    #         return formataddr((self.env.user.name, self.env.user.email))
+    #     raise UserError(_("Unable to send email, please configure the sender's email address or alias."))
+    #
+    # def create_mail_message(self, body):
+    #     user = self.env.user
+    #     for move in self:
+    #         vals = {'message_type': 'notification',
+    #                 'author_id': user.partner_id.id,
+    #                 'date': datetime.now(),
+    #                 'email_from': self._get_default_from(),
+    #                 'model': 'account.move',
+    #                 'res_id': move.id,
+    #                 'subtype_id': 2,
+    #                 'body': body}
+    #         self.env['mail.message'].create(vals)
+
 
